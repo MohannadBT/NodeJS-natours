b61b3e5581f7deac1e2003c19e5f1b26001367c3
MohannadBT <xsmile3x@gmail.com>
2025-08-14 19:19:47 +0300

    lec 138

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,6 +39,15 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password') || this.isNew) next();
+
+  this.passwordChangedAt = Date.now() - 1000; // making it 1s behind insure password changed before token issued
+  next();
 });
 
 userSchema.pre('save', async function (next) {
@@ -68,6 +78,19 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   // False means not changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
