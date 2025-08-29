9f0baf5571ee62892dd07701616c6884ecd0ec59
MohannadBT <xsmile3x@gmail.com>
2025-08-29 22:27:10 +0300

    Setting Security HTTP Headers

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,8 +13,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Global Middlewares
+// Set security HTTPS headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -19,8 +27,25 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS (cross-site-scripting)
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+
+  next();
+});
 
 // 3) routes
 app.use('/api/v1/tours', tourRouter); // this one is called mounting the router
