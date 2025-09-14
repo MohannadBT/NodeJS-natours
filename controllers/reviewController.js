efdfa7a7e97e4bcdc000655d27eb7b628a8c7cc5
MohannadBT <xsmile3x@gmail.com>
2025-09-14 10:06:38 +0300

    Factory Functions: - Update - Create

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,6 +2,14 @@ const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/reviewModel');
 const factory = require('./handlerFactory');
 
+// we moved the setting of the data into a middleware and we link it in the reviewRoutes
+exports.setTourUserId = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
   if (req.params.tourId) filter = { tour: req.params.tourId };
@@ -15,17 +23,8 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  // Allow nested routes-
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
+exports.createReview = factory.createOne(Review);
 
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: { review: newReview },
-  });
-});
+exports.updateReview = factory.updateOne(Review);
 
 exports.deleteReview = factory.deleteOne(Review);
