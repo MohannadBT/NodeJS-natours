0ddbe3cdc1b38c4ee5319eb7079ad5af09c988d3
MohannadBT <xsmile3x@gmail.com>
2026-01-05 20:32:32 +0300

    Stripe doesnt work in Saudi Arabia, so this part is mostly broken :>

---
+++ public/js/stripe.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe(process.env.STRIPE_PUBLIC_KEY);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`,
+    );
+
+    // 2) crteate checkout form + cahrge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    showAlert('error', err);
+  }
+};
